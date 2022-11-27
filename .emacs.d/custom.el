4003dfc9225850ad63fed861e83ec57fef53467d
Oliver Mead <olivermead@olivermead.xyz>
2022-11-27 16:32:24 +0000

    org contrib, python and formatting.

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -17,10 +17,23 @@
       (sign)
       (encrypt))))
  '(mml-secure-openpgp-sign-with-sender t)
+ '(org-agenda-files '("~/git/nerf/nerf.org"))
  '(package-selected-packages
-   '(astyle flycheck company-irony irony pinentry emmet-mode tramp-nspawn toc-org geiser-guile geiser org-plus-contrib org-contrib sly desktop-environment exwm simple-httpd gnus-desktop-notify gnus-notify exec-path-from-shell eglot haskell-mode yasnippet company yasnippet-snippets evil-magit counsel-projectile transmission beacon debbugs emojify multi-vterm vterm multi-term-tmux multi-term general which-key use-package undo-tree treemacs-tab-bar treemacs-projectile treemacs-persp treemacs-magit treemacs-icons-dired treemacs-evil rainbow-delimiters pass parinfer-rust-mode ivy-rich ivy-pass helm evil-surround evil-snipe evil-commentary evil-collection doom-themes doom-modeline counsel command-log-mode circe all-the-icons))
+   '(format-all lsp-jedi ox-extra poetry astyle flycheck company-irony irony pinentry emmet-mode tramp-nspawn toc-org geiser-guile geiser org-plus-contrib org-contrib sly desktop-environment exwm simple-httpd gnus-desktop-notify gnus-notify exec-path-from-shell eglot haskell-mode yasnippet company yasnippet-snippets evil-magit counsel-projectile transmission beacon debbugs emojify multi-vterm vterm multi-term-tmux multi-term general which-key use-package undo-tree treemacs-tab-bar treemacs-projectile treemacs-persp treemacs-magit treemacs-icons-dired treemacs-evil rainbow-delimiters pass parinfer-rust-mode ivy-rich ivy-pass helm evil-surround evil-snipe evil-commentary evil-collection doom-themes doom-modeline counsel command-log-mode circe all-the-icons))
  '(safe-local-variable-values
-   '((diff-add-log-use-relative-names . t)
+   '((eval org-babel-do-load-languages 'org-babel-load-languages
+           '((shell . t)
+             (python . t)))
+     (org-num-skip-unnumbered . t)
+     (eval setenv "HSA_OVERRIDE_GFX_VERSION" "10.3.0")
+     (eglot-server-programs
+      (python-mode "/home/olivermead/git/scratch/nerf/.env/bin/pyls"))
+     (python-shell-interpreter . "/home/olivermead/git/scratch/nerf/.env/bin/python")
+     (company-backends . company-capf)
+     (eglot-server-programs
+      (python-mode "./.env/bin/pyls"))
+     (python-shell-interpreter . "./env/bin/python")
+     (diff-add-log-use-relative-names . t)
      (vc-git-annotate-switches . "-w"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
